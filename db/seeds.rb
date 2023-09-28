127ebcdb353e11a707d83347e502748ac76b42d3
Dat Dinh <trongdatdinh1@gmail.com>
2023-09-28 11:25:54 +0700

    Provide README

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@
 
 AdminUser.create!(email: 'admin@example.com', password: 'password') if Rails.env.development?
 Setting.create_or_find_by!(key: 'min_version', value: '0.0')
+
+User.create!(email: 'user@example.com', password: 'password') if Rails.env.development?
